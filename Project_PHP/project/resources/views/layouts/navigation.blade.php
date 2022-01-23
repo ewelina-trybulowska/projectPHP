78f9fdf7e1eebafcce360c73aae3d9031eea99c4
Dorota Zub <dzub@student.agh.edu.pl>
2022-01-23 21:55:48 +0100

    Adding admin tests

--- Project_PHP/project/resources/views/layouts/navigation.blade.php
+++ Project_PHP/project/resources/views/layouts/navigation.blade.php
@@ -38,9 +38,9 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
+                            <x-dropdown-link id="logout" :href="route('logout')"
                                     onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                                this.closest('form').submit(); ">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
